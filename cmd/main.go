fef56766118ae6ccbb6b321ac2cc4f9d66ed412d
Yerzhan <omnilumiere@gmail.com>
2023-08-14 17:04:32 +0600

    Add dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,28 @@ import (
 
 	"github.com/ryodanqqe/todo-app"
 	"github.com/ryodanqqe/todo-app/pkg/handler"
+	"github.com/ryodanqqe/todo-app/pkg/repository"
+	"github.com/ryodanqqe/todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	srv := new(todo.Server)
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("failed to initialize configs due to error: %s", err.Error())
+	}
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("failed to run server due to error: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
